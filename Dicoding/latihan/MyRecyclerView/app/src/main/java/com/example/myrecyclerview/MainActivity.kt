656f04b17094c6ee3821064b87efba72fafd8337
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-26 20:53:29 +0800

    update: create fun showRecyclerGrid

--- Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -34,16 +35,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun setMode(selectedMode: Int) {
         when(selectedMode){
-            R.id.action_list -> {}
-            R.id.action_grid -> {}
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+                showRecyclerGrid()
+            }
             R.id.action_cardview -> {}
         }
-
     }
 
-    private fun showRecylerList(){
+    private fun showRecyclerList(){
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    private fun showRecyclerGrid(){
+        rvHeroes.layoutManager = GridLayoutManager(this,2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+
     }
 }
