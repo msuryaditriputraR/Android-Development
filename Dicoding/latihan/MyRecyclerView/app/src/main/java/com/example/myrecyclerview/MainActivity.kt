bdc724fc46d8e9147050da8932824924db99f63b
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-26 16:18:51 +0800

    add: MainActivity

--- Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+
+    }
+
+    private fun showRecylerList(){
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
