010b77f61472009120095313b11ca1e22c00be86
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-26 16:32:46 +0800

    update: MainActivity

--- Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,6 +19,25 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.setHasFixedSize(true)
 
         list.addAll(HeroesData.listData)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode){
+            R.id.action_list -> {}
+            R.id.action_grid -> {}
+            R.id.action_cardview -> {}
+        }
 
     }
 
