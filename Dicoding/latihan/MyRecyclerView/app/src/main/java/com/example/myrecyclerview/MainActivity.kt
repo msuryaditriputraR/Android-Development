913c0dbcc6e1b6a368188d79e3aa99fa007df28f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-26 21:44:45 +0800

    add: default value of action bar when first start

--- Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ Dicoding/latihan/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBarTitle(title)
 
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
